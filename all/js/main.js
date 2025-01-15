d1d8257ce7a8bec1cfd6857ca99d80662c9950f9
Achiepo <yvanachiepo@icloud.com>
2025-01-15 11:02:45 +0100

    PORTFOLIO

--- all/js/main.js
+++ all/js/main.js
@@ -1,16 +1,7 @@
 
 
-
-
-
-
-
-
 (function() {
   "use strict";
-
-  
-
 
   const mobileNavToggleBtn = document.querySelector('.mobile-nav-toggle');
 
@@ -118,21 +109,4 @@
   /**
    * Navmenu Scrollspy
    */
-  let navmenulinks = document.querySelectorAll('.navmenu a');
-
-  function navmenuScrollspy() {
-    navmenulinks.forEach(navmenulink => {
-      if (!navmenulink.hash) return;
-      let section = document.querySelector(navmenulink.hash);
-      if (!section) return;
-      let position = window.scrollY + 200;
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        document.querySelectorAll('.navmenu a.active').forEach(link => link.classList.remove('active'));
-        navmenulink.classList.add('active');
-      } else {
-        navmenulink.classList.remove('active');
-      }
-    })
-  }
-
 })();
